6f95bf4b75cae0e48a71baeaad3cd6c8cf8d9da9
shubham25503 <shubhamdp.255@gmail.com>
2023-05-27 16:48:06 +0530

    Update PIDRatio.cpp

--- PIDRatio/PIDRatio.cpp
+++ PIDRatio/PIDRatio.cpp
@@ -18,7 +18,8 @@ public:
     double xKp = 1.0, xKi = 0, xKd = 0;
     double yKp = 1.0, yKi = 0, yKd = 0;
     double rKp = 3.0, rKi = 0, rKd = 0;//3.0
-    double brakeKp = 4.0, brakeKi = 0.1, brakeKd = 0;
+    double brakeKp = 4.0, brakeKi = 0.0, brakeKd = 0;
+    int feedbackOffset = 8;
     PID *fxPID, *fyPID, *frPID, *brake;
 
     PIDRatio() {}
@@ -52,15 +53,57 @@ public:
         brake->SetSampleTime(1);
 
         fxPID->SetOutputLimits(-255, 255);
-        fyPID->SetOutputLimits(-255, 255);
+        fyPID->SetOutputLimits(-200, 200);
         frPID->SetOutputLimits(-255, 255);
         brake->SetOutputLimits(-255, 255);
     }
+    
+    void updateYPID(double yKp,double yKi,double yKd)
+    {
+        this-> yKp = yKp;
+        this-> yKi = yKi;
+        this-> yKd = yKd;
+        fyPID->SetTunings(this->yKp,this->yKi,this->yKd);
+        // Serial.println((String)this->yKp+", "+(String)this->yKi+", "+(String)this->yKd);
+    }
+    void updateRPID(double rKp,double rKi,double rKd)
+    {
+        this-> rKp = rKp;
+        this-> rKi = rKi;
+        this-> rKd = rKd;
+        frPID->SetTunings(this->rKp,this->rKi,this->rKd);
+        // Serial.println((String)this->yKp+", "+(String)this->yKi+", "+(String)this->yKd);
+    }
+    void updateXPID(double xKp,double xKi,double xKd)
+    {
+        this-> xKp = xKp;
+        this-> xKi = xKi;
+        this-> xKd = xKd;
+        fxPID->SetTunings(this->xKp,this->xKi,this->xKd);
+        // Serial.println((String)this->yKp+", "+(String)this->yKi+", "+(String)this->yKd);
+    }
+
+    void updateBrakePID(double brakeKp,double brakeKi,double brakeKd)
+    {
+        this-> brakeKp = brakeKp;
+        this-> brakeKi = brakeKi;
+        this-> brakeKd = brakeKd;
+        brake->SetTunings(brakeKp,brakeKi,brakeKd);
+        Serial.println((String)this->brakeKp+", "+(String)this->brakeKi+", "+(String)this->brakeKd);
+    }
+
+    void setFeedbackOffset(int off)
+    {
+        this-> feedbackOffset = off;
+        Serial.println("feed: "+(String)this->feedbackOffset);
+    }
+
     void compute()
     {
             // UserIn->display();
+            // _feedback->display();
             UserIn->process();
-
+            // Serial.println(yKp);
             // Braking Logic
             if (UserIn->isZero && !prevUserIn->isZero)// Detecting Brake
             {
@@ -80,12 +123,14 @@ public:
             *prevUserIn = *currUserIn;
 
 
-            *t_feedback = *_feedback / 8; // test this
+            *t_feedback = *_feedback / feedbackOffset; // test this // 8
             *currentFeedback = *t_feedback - *prevFeedback;
             *speedReferenceFeedback = *currentFeedback;
             currUserIn->process();
             if (!currUserIn->isZero)
             {
+                // Serial.println("!isZero");
+                // delay(100000);
                 opt->minimize();
             }
             currUserIn->process();
@@ -106,5 +151,29 @@ public:
             fxPID->Compute();
             fyPID->Compute();
             frPID->Compute(); 
+            // output->display();
     }
-} PID_ratio;
+
+    Direction *equatePrev()
+    {
+        if(smartBase.distanceMode){
+            _feedback->fy=feedback.valueY;
+            _feedback->fr=feedback.valueR;
+            *t_feedback = *_feedback / feedbackOffset;
+            *prevFeedback= *t_feedback;
+            *currentFeedback = *t_feedback - *prevFeedback;
+            return currentFeedback;
+        }
+        else if(!smartBase.distanceMode)
+        {
+            *t_feedback = *_feedback / feedbackOffset;
+            *prevFeedback= *t_feedback;
+            *currentFeedback = *t_feedback - *prevFeedback;
+            return currentFeedback;
+        }
+    }
+    void storePrev(Direction *_current)
+    {
+        *currentFeedback = *_current;
+    }
+} PID_ratio,SB_ratio;
